f45c99327aad3b38b6a4cd0fa2a63ded1993bcf7
mohd-shabeer <mohammedshabeer2520@gmail.com>
2026-05-14 04:00:09 +0530

    fix: image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -8,12 +8,18 @@ const nextConfig: NextConfig = {
         hostname: "images.pexels.com",
       },
       {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
         protocol: "http",
         hostname: "work.phpwebsites.in",
+        pathname: "/gobite/photos/**",
       },
       {
         protocol: "https",
         hostname: "work.phpwebsites.in",
+        pathname: "/gobite/photos/**",
       },
     ],
   },
